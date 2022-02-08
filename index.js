13bebe3f2b8c0e6fd94068473a6764257d78242e
brhavos <brhavos@gmail.com>
2022-02-08 19:28:44 -0300

    ajustes finais

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send('Node - Simple Project - Hello ${name}!');
+  res.send(`Node Simple Project\nHello ${name}!`);
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log('helloworld: listening on port ${port}');
+  console.log(`------------------------------------------------\nNode Simple Project\n
+  Hello World! - rodando: listening on port ${port} \n
+  by Carlos Ciccolani\n------------------------------------------------
+  `);
 });
